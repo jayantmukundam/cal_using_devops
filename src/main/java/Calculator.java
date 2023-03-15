a1ee1b21b8439a69bee4d95f941b896643f56ef1
jayantmukundam <jayantmukundam2811@gmail.com>
2023-03-15 16:21:19 +0530

    created test for all the functions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,6 +13,30 @@ public class Calculator {
         return ans;
     }
 
+    static int findFactorial(int num) {
+        logger.info("FACTORIAL OF "+ num);
+        int ans = 1;
+        for(int i=1;i<=num;i++)
+            ans*=i;
+        logger.info("RESULT "+ans);
+        return ans;
+
+    }
+
+    static double findLog(double num) {
+        logger.info("LOG OF "+num);
+        double ans = Math.log(num);
+        logger.info("RESULT "+ans);
+        return ans;
+    }
+
+    static double findPower(double base, double exponent) {
+        logger.info("BASE "+base + " | EXPONENT "+exponent);
+        double ans = Math.pow(base,exponent);
+        logger.info("RESULT "+ans);
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -38,14 +62,41 @@ public class Calculator {
                 break;
                 case 2:{
                     //factorial
+                    System.out.println("Enter a non-negative integer whose factorial you want:");
+                    int input = sc.nextInt();
+                    if(input<0){
+                        System.out.println("Factorial is not applicable to the given input\n");
+                        break;
+                    }
+                    int ans = findFactorial(input);
+                    System.out.println("Factorial of "+input+" is "+ans+"\n");;
                 }
                 break;
                 case 3:{
                     // log
+                    System.out.println("Enter a positive integer whose natural log is required:");
+                    double input = sc.nextDouble();
+                    if(input<=0){
+                        System.out.println("Natural log is not applicable to the given input\n");
+                        break;
+                    }
+                    double ans = findLog(input);
+                    System.out.println("Natural log of "+input+" is "+ans+"\n");
+
                 }
                 break;
                 case 4:{
                     // power
+                    System.out.println("Enter the base number:");
+                    double base = sc.nextDouble();
+                    sc.nextLine();
+
+                    System.out.println("Enter the exponent number:");
+                    double exponent = sc.nextDouble();
+
+                    double ans = findPower(base,exponent);
+                    System.out.println(base+" raised to "+exponent+" is "+ans+"\n");
+
                 }
                 break;
                 case 5:{
@@ -58,4 +109,8 @@ public class Calculator {
 
 
     }
+
+
+
+
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -6,9 +6,10 @@ import static org.junit.Assert.*;
 public class CalcTest {
 
     Calculator calc = new Calculator();
-    private static final double delta = 1e-6;
+    private static final double delta = 1e-15;
 
 
+    // SQUARE ROOT
     @Test
     public void sqrtTruePositive() {
         assertEquals("Finding square root for True Positive", 6.0, calc.findSqrt(36),delta);
@@ -17,6 +18,41 @@ public class CalcTest {
     @Test
     public void sqrtTrueNegative() {
         assertNotEquals("Finding square root for True Negative", 6.0, calc.findSqrt(49),delta);
+    }
+
+    // FACTORIAL
+    @Test
+    public void factorialTruePositive() {
+        assertEquals("Finding factorial for True Positive", 720, calc.findFactorial(6),delta);
+    }
+
+    @Test
+    public void factorialTrueNegative() {
+        assertNotEquals("Finding factorial for True Negative", 2, calc.findFactorial(5),delta);
+    }
+
+
+    // LOG
+    @Test
+    public void logTruePositive() {
+        assertEquals("Finding log for True Positive", 0, calc.findLog(1),delta);
+    }
+
+    @Test
+    public void logTrueNegative() {
+        assertNotEquals("Finding log for True Negative", 1.0, calc.findLog(2),delta);
+    }
+
+
+    // POWER
+    @Test
+    public void powerTruePositive() {
+        assertEquals("Finding power for True Positive", 9.0, calc.findPower(3,2),delta);
+    }
+
+    @Test
+    public void powerTrueNegative() {
+        assertNotEquals("Finding power for True Negative", 64.0, calc.findPower(4,2),delta);
     }
 
 }
